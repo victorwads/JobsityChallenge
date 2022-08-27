20a01bcc35d14e4c45149ce073aaa34d2612e0a2
Victor Wads <victor.laureano@gmail.com>
2022-08-27 16:34:37 -0300

    chore(listing): init retrofit config

---
+++ app/src/main/java/br/com/victorwads/job/vicflix/commons/repositories/retrofit/RetrofitProvider.kt
@@ -0,0 +1,22 @@
+package br.com.victorwads.job.vicflix.commons.repositories.retrofit;
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object RetrofitProvider {
+
+    val instance: Retrofit
+        get() = retrofit ?: initInstance()
+
+    private var retrofit: Retrofit? = null
+
+    private fun initInstance(): Retrofit = with(Retrofit.Builder()) {
+        baseUrl(API_BASE_URL)
+        addConverterFactory(GsonConverterFactory.create())
+        build()
+    }
+
+    private const val API_BASE_URL = "https://api.tvmaze.com/"
+
+}
