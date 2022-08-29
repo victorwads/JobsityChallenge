28a734118ce8f4cf42f6c9467fe4e9568e5cfe02
Victor Wads <victor.laureano@gmail.com>
2022-08-28 22:05:30 -0300

    chore(settings): save user auth preferences

--- app/src/main/kotlin/br/com/victorwads/job/vicflix/features/settings/view/SettingsActivity.kt
+++ app/src/main/kotlin/br/com/victorwads/job/vicflix/features/settings/view/SettingsActivity.kt
@@ -1,5 +1,26 @@
 package br.com.victorwads.job.vicflix.features.settings.view
 
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import br.com.victorwads.job.vicflix.R
 import br.com.victorwads.job.vicflix.commons.view.BaseActivity
 
-class SettingsActivity : BaseActivity()
+class SettingsActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.frame_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.settings_preferences, rootKey)
+        }
+    }
+}
